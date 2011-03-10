4e72d219519c6276542e32daa6504f9cd05ee103
joelbasson <joelbasson@me.com>
2011-03-10 05:15:12 +0200

    initial mobile styling

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,6 +6,7 @@ class TransactionsController < ApplicationController
     @transactions = current_user.wallet.transactions.paginate(:per_page => 2, :page => params[:page])
     respond_with(@transactions) do |format|
       format.html { redirect_to current_user.wallet }
+    end  
   end
 
 end
